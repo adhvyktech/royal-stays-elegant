930a789eb21bd94c4d3d63a42c35fb1255bbdcb5
adhvyktech <adhvyktech@gmail.com>
2026-04-18 12:52:47 +0530

    seo update

--- src/components/MapSection.tsx
+++ src/components/MapSection.tsx
@@ -13,7 +13,7 @@ const MapSection = () => {
       </div>
       <div className="w-full h-[400px] md:h-[450px]">
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.716447854842!2d77.59428517507779!3d12.988259187329836!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1676e98a1249%3A0x9c3c0e9d0f6c8e8a!2sShivajinagar%2C%20Bengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1706000000000!5m2!1sen!2sin"
+          src="https://maps.google.com/maps?q=Royal%20Inn%20Lodge,%20N.K.P.road,%20shivajinagar,%20Bangalore&t=&z=15&ie=UTF8&iwloc=&output=embed"
           width="100%"
           height="100%"
           style={{ border: 0 }}
